05fac46232571bad76ab0c310c04c0d64433dc25
Jarod Wilson <jaroddwilson@gmail.com>
2021-08-30 09:05:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const newArray = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return newArray;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int--);
+    }
+}
